a891807641620eedd31ca6938ad140cc621cf087
Esin <nbusra.esin@gmail.com>
2023-04-04 17:00:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+const eso = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function (drivers){
+    let firstTwo = drivers.slice(0,2)
+    return firstTwo
+}
+
+const returnLastTwoDrivers = function (drivers){
+    let lastTwo = drivers.slice(-2)
+    return lastTwo
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare*multiplier
+    }
+
+}
+
+const double = createFareMultiplier(2)
+const triple = createFareMultiplier(3)
+
+function fareDoubler(fare){
+    return double(fare)
+}
+
+function fareTripler(fare){
+    return triple(fare)
+}
+
+function selectDifferentDrivers(drivers, cb){
+    return cb(drivers)
+}
+
